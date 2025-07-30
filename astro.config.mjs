acd4681bba1435ae7c00b769440a77e66d7a5046
Miguel <31miguelop31@gmail.com>
2025-07-30 13:51:22 -0500

    tercer cambio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Migueljcm16.github.io',
-    base: 'Portafolio2025',
+    /*site: 'https://Migueljcm16.github.io',
+    base: 'Portafolio2025',*/
     devToolbar: {
         enabled: false,
     },

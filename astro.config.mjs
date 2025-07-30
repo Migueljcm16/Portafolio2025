d194e7975835917c3bdb447228f1b4b268cf2936
Miguel <31miguelop31@gmail.com>
2025-07-30 12:02:30 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://Migueljcm16.github.io',
+    base: 'Portafolio2025',
+    devToolbar: {
+        enabled: false,
+    },
+});
